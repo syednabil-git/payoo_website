7462b87b8819b27a933727e22c09c3e2198e91a8
Syed Muhammad Nabil <111644246+syednabil-git@users.noreply.github.com>
2025-10-01 13:39:25 +0600

    add money, cashout, transactions section done

--- home.js
+++ home.js
@@ -1,13 +1,60 @@
 const validPin = 1234
+const transactionData = []
 
+// function to get input value
+function getInputValueNumber (id){
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    const inputFieldValueNumber = parseInt(inputFieldValue)
+    return inputFieldValueNumber
+}
+
+function getInputValue (id){
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    return inputFieldValue
+}
+// function to get innertext
+function getInnerText(id){
+    const element = document.getElementById(id)
+    const elementValue = element.innerText
+    const elementValueNumber = parseInt(elementValue)
+    return elementValueNumber
+}
+// function to set innertext
+function setInnerText(value){
+    const availableBalanceElement = document.getElementById("available-balance")
+    availableBalanceElement.innerText = value
+}
+//function to toggle
+function handleToggle(id){
+    const forms = document.getElementsByClassName("form")
+    for(const form of forms){
+        form.style.display ="none"
+    }
+    document.getElementById(id).style.display="block"
+}
+// function to toggle buttons
+function handleButtonToggle(id){
+    const formBtns = document.getElementsByClassName("form-btn")
+
+    for(const btn of formBtns){
+        btn.classList.remove("border-[#0874f2]","bg-[#0874f20d]")
+        btn.classList.add("border-gray-300")
+    }
+    document.getElementById(id).classList.remove("border-gray-300")
+      document.getElementById(id).classList.add("border-[#0874f2]","bg-[#0874f20d]")
+}
+
+// add money section
 document.getElementById("add-money-btn").addEventListener("click", function(e){
     e.preventDefault()
     console.log("add money btn")
-    const bank = document.getElementById("bank").value
-    const accountNumber = document.getElementById("bank-account").value
-    const amountAdd = parseInt(document.getElementById("amount").value)
-    const pinAdd = parseInt(document.getElementById("pin").value)
-    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+    const bank = getInputValue ("bank")
+    const accountNumber = getInputValue("bank-account")
+    const amountAdd = getInputValueNumber("amount")
+    const pinAdd = getInputValueNumber("pin")
+    const availableBalance = getInnerText("available-balance")
     if(accountNumber.length < 6){
         alert("please provide a valid account number")
         return;
@@ -17,16 +64,23 @@ document.getElementById("add-money-btn").addEventListener("click", function(e){
         return;
     }
     const totalNewAvailableBalance = amountAdd+availableBalance
-    document.getElementById ("available-balance").innerText = totalNewAvailableBalance
+    setInnerText(totalNewAvailableBalance)
+
+    const data = {
+        name: "Add Money",
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data)
+    console.log(transactionData)
 })
 
 // cashout money feature 
 document.getElementById("withdraw-btn").addEventListener("click", function(e){
 e.preventDefault()
-const agentNumber = document.getElementById("agent-number").value
-const pinAdd = parseInt(document.getElementById("withdrew-pin").value)
-const amount = parseInt(document.getElementById("withdrew-amount").value)
-const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+const agentNumber = getInputValue("agent-number")
+const pinAdd = getInputValueNumber("withdrew-pin")
+const amount = getInputValueNumber("withdrew-amount")
+const availableBalance = getInnerText("available-balance")
 if(agentNumber.length < 6){
         alert("please provide a valid account number")
         return;
@@ -36,16 +90,65 @@ if(agentNumber.length < 6){
         return;
     }
 const totalNewAvailableBalance = availableBalance - amount
-document.getElementById("available-balance").innerText = totalNewAvailableBalance
+setInnerText(totalNewAvailableBalance)
+
+ const data = {
+        name: "Cash Out",
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data)
+    console.log(transactionData)
+})
+
+document.getElementById("transaction-button").addEventListener("click",function(){
+    const transactionContainer = document.getElementById("transaction-container")
+    transactionContainer.innerText = ""
+
+    for(const data of transactionData){
+        const div = document.createElement("div")
+        div.innerHTML=`
+         <div class="bg-white rounded-xl p-3 flex justify-between items-center mt-3">
+            <div class="flex items-center">
+              <div class=" p-3 rounded-full bg-[#f4f5f7]">
+                <img src="./assets/wallet1.png" class="mx-auto"/>
+              </div>
+              <div>
+                <h1>${data.name}</h1>
+                <p>${data.date}</p>
+              </div>
+
+            </div>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+          </div>
+        `
+        transactionContainer.appendChild(div)    
+    }
 })
 
 
 // toggle feature
 document.getElementById("add-button").addEventListener("click", function(){
-    document.getElementById("cash-out-parent").style.display = "none"
-    document.getElementById("add-money-parent").style.display = "block"
+  const forms = document.getElementsByClassName("form") 
+  handleToggle("add-money-parent")
+  handleButtonToggle("add-button")
 })
 document.getElementById("cashout-button").addEventListener("click", function(){
-    document.getElementById("add-money-parent").style.display = "none"
-    document.getElementById("cash-out-parent").style.display = "block"
+  handleToggle("cash-out-parent")
+  handleButtonToggle("cashout-button")  
+})
+document.getElementById("transfermoney-button").addEventListener("click", function(){
+  handleToggle("transfer-money-parent")
+  handleButtonToggle("transfermoney-button") 
+})
+document.getElementById("bonus-button").addEventListener("click", function(){
+  handleToggle("get-bonus-parent")
+  handleButtonToggle("bonus-button") 
+})
+document.getElementById("paybill-button").addEventListener("click", function(){
+  handleToggle("pay-bill-parent")
+  handleButtonToggle("paybill-button") 
+})
+document.getElementById("transaction-button").addEventListener("click", function(){
+  handleToggle("transaction-parent")
+  handleButtonToggle("transaction-button") 
 })
